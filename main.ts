ad1e7774b3e5e2442ecde5768c8383dafde0ef0d
Raul Calderon <raul41092@gmail.com>
2025-06-21 12:24:08 -0700

    Added some descriptions

--- main.ts
+++ main.ts
@@ -2,14 +2,18 @@ import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 import { z } from 'zod'
 
-// 1.- Create server
+// 1.- Create server.
+// Main interface for the MCP server.
+// It manages the tools, connections, and interactions with clients.
 
 const server = new McpServer({
     name: "Demo",
     version: "1.0.0"
 })
 
-// 2.- Set tools
+// 2.- Set tools.
+// Tools are functions that can be called by clients to perform specific tasks.
+// In this case, we create a tool to fetch the weather of a city.
 
 server.tool(
     'fetch-weather', // titulo de la herramienta
@@ -50,7 +54,9 @@ server.tool(
     }
 )
 
-// 3.- Escuchar conexiones de cliente
+// 3.- Listen for connections.
+// The server listens for incoming connections from clients.
+// In this case, we use a StdioServerTransport to communicate via standard input/output.
 const transport = new StdioServerTransport()
 await server.connect(transport)
 
